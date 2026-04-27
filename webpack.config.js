2c386bab966957ffe03c27a4d1e39905744b74aa
VictorDMoura <mouramvictor@gmail.com>
2026-04-27 11:04:06 -0300

    chore: add webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
-const path = require("path")
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    target: "web",
-    mode: "development",
+  target: "web",
+  mode: "development",
 
-    entry: path.resolve(__dirname, "src", "main.js"),
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
+  entry: path.resolve(__dirname, "src", "main.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
     },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
 
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "dist")
-        },
-        port: 3000,
-        open: true,
-        liveReload: true,
-    },     
-}
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
