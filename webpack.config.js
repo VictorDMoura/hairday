51792a31c886d2e0f65afd6505b0ecc3928ab238
VictorDMoura <mouramvictor@gmail.com>
2026-04-27 11:06:53 -0300

    feat: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve(__dirname, "src", "assets", "scissors.svg"),
     }),
   ],
 };
