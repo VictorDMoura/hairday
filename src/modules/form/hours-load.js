1f658faab9aa121a49ef8f13df2e0b4c0ecc427f
VictorDMoura <mouramvictor@gmail.com>
2026-04-29 10:20:31 -0300

    feat: add hour-selected class only on click

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,6 @@
 import { openingHours } from "../../utils/opening-hours.js";
 import dayjs from "dayjs";
+import { hoursClick } from "./hours-click.js";
 
 const hours = document.getElementById("hours");
 
@@ -37,6 +38,9 @@ export function hoursLoad({ date }) {
 
     hours.append(li);
   });
+
+  //Adiciona o evento de clique nos horários disponíveis
+  hoursClick();
 }
 
 function hourHeaderAdd(title) {
