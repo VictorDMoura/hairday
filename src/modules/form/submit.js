406d75f5487f7cadb55cedfa2d9af8bd2635dd50
VictorDMoura <mouramvictor@gmail.com>
2026-04-29 10:33:46 -0300

    feat: add save schedule date

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
 // Data atual para o formatar o input
@@ -16,5 +17,38 @@ form.onsubmit = (event) => {
   //Previne o comportamente do formulário (recarregar a página)
   event.preventDefault();
 
-  console.log("ENVIADO");
+  try {
+    //Recuperando o nome do cliente
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Informe o nome do cliente");
+    }
+
+    // recuperar o horário selecionado
+    const hourSelected = document.querySelector(".hour-selected");
+
+    // Recupera o horário selecionado
+    if (!hourSelected) {
+      return alert("Selecione a hora.");
+    }
+
+    // Recuperar somente a hora
+    const [hour, _] = hourSelected.innerText.split(":");
+
+    //Inserir a hora na data
+
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    // Gerar um ID
+    const id = new Date().getTime();
+    console.log({
+      id,
+      name,
+      when,
+    });
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento.");
+    console.log(error);
+  }
 };
